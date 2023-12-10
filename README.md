0b5467c17ba6c140298fe4c8f4d88e7c7d1beb3b
Edwin de los Santos <edwin2delossantos@gmail.com>
2023-12-10 17:27:28 -0400

    add script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# odoo-docker-install
-Odoo install script
+# [Odoo](https://www.odoo.com "Odoo's Homepage") Install Script
+## Installation procedure
+
+##### 1. Download the script:
+```
+sudo wget https://raw.githubusercontent.com/fow903/odoo-docker-install/12.0/odoo_install.sh
+```
+##### 2. Modify the parameters as you wish.
+There are a few things you can configure, this is the most used list:<br/>
+```OE_USER``` will be the username for the system user.<br/>
+```ODOO_VERSION``` is the Odoo version to install, for example ```12.0``` for Odoo V12.<br/>
+```POSTGRES_VERSION``` is the Postgres version to install, for example ```15``` for Postgres V15.<br/>
+#### 3. Make the script executable
+```
+sudo chmod +x odoo_install.sh
+```
+##### 4. Execute the script:
+```
+sudo ./odoo_install.sh
+```
---
+++ odoo_install.sh
@@ -0,0 +1,65 @@
+OE_USER="odoo"
+OE_HOME="/$OE_USER"
+POSTGRES_USER=$OE_USER
+POSTGRES_PASSWORD="postgres"
+POSTGRES_CONTAINER_NAME="db"
+ODOO_CONTAINER_NAME="odoo"
+ODOO_VERSION="12.0"
+POSTGRES_VERSION="15"
+
+echo -e "\n---- Create ODOO system user ----"
+sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
+
+sudo adduser $OE_USER sudo
+
+echo -e "\n---- Create Posgres data folder ----"
+
+DATA_PATH="/$OE_HOME/.data"
+sudo mkdir $DATA_PATH
+
+echo -e "\n---- Create Posgres container ----"
+
+docker run -d \
+	--name $POSTGRES_CONTAINER_NAME \
+	-e POSTGRES_USER=$POSTGRES_USER \
+	-e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    -e POSTGRES_DB=postgres \
+	-e PGDATA=/var/lib/postgresql/data/pgdata \
+	-v $DATA_PATH:/var/lib/postgresql/data \
+	postgres:$POSTGRES_VERSION
+
+echo -e "\n---- Create Log directory ----"
+LOG_PATH="/var/log/$OE_USER"
+sudo mkdir $LOG_PATH
+sudo chown $OE_USER:$OE_USER $LOG_PATH
+
+sudo chown 101 $FILESTORE_DIR
+
+echo -e "\n---- Create Config directory ----"
+
+CONFIG_PATH="/$OE_HOME/config"
+sudo mkdir $CONFIG_PATH
+
+echo -e "\n---- Create Addons directory ----"
+
+ADDONS_DIR="/$OE_HOME/extra-addons"
+sudo mkdir $ADDONS_DIR
+
+echo -e "\n---- Create Filestore directory ----"
+
+FILESTORE_DIR="/$OE_HOME/OdooData"
+sudo mkdir $FILESTORE_DIR
+
+sudo chown 101 $FILESTORE_DIR
+
+echo -e "\n---- Create Odoo container ----"
+
+docker run -d \
+    -p 8069:8069 -p 8072:8072 \
+    -v $CONFIG_PATH:/etc/odoo \
+    -v $ADDONS_DIR:/mnt/extra-addons \
+    -v $LOG_PATH:/var/log/odoo \
+    -v $FILESTORE_DIR:/var/lib/odoo \
+    --name $ODOO_CONTAINER_NAME \
+    --link $POSTGRES_CONTAINER_NAME:$POSTGRES_CONTAINER_NAME \
+    -t odoo:$ODOO_VERSION
