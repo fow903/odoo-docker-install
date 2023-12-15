4a60a209eba34b70b694698da56390549268a96f
Edwin de los Santos <edwin2delossantos@gmail.com>
2023-12-15 17:44:42 -0400

    remove platform for intel install

--- macos_odoo_install_intel.sh
+++ macos_odoo_install_intel.sh
@@ -1,3 +1,5 @@
+. ./functions.sh
+
 INSTALL_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 INSTALL_PATH="$(cd -- "$INSTALL_PATH" && pwd)"
 if [[ -z "$INSTALL_PATH" ]] ; then
@@ -12,58 +14,52 @@ ODOO_CONTAINER_NAME="odoo"
 ODOO_VERSION="16.0"
 POSTGRES_VERSION="15"
 
-echo "\n---- Create Posgres data folder ----"
+echo "\n---- Create Postgres data folder ----"
 
 DATA_PATH="/$OE_HOME/.data"
-sudo mkdir $DATA_PATH
+create_directory $DATA_PATH
 
-echo "\n---- Create Posgres container ----"
+sudo chown 999 $DATA_PATH
+
+echo "\n---- PostgreSQL data path directory created successfully! ----"
+
+echo "\n---- Create Postgres container ----"
 
 docker run -d \
-	--name $POSTGRES_CONTAINER_NAME \
-	-e POSTGRES_USER=$POSTGRES_USER \
-	-e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+    --name $POSTGRES_CONTAINER_NAME \
+    -e POSTGRES_USER=$POSTGRES_USER \
+    -e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
     -e POSTGRES_DB=postgres \
-	-e PGDATA=/var/lib/postgresql/data/pgdata \
-	-v $DATA_PATH:/var/lib/postgresql/data \
-	postgres:$POSTGRES_VERSION
+    -e PGDATA=/var/lib/postgresql/data:z \
+    -v $DATA_PATH:/var/lib/postgresql/data \
+    postgres:$POSTGRES_VERSION
+
+# Check the exit status of the last command (docker run)
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to run the PostgreSQL container."
+    exit 1
+fi
+
+echo "\n---- PostgreSQL container created successfully! ----"
 
 echo "\n---- Create Log directory ----"
 LOG_PATH="$OE_HOME/log"
-sudo mkdir $LOG_PATH
-sudo chown $OE_USER:$OE_USER $LOG_PATH
 
-sudo chown 101 $LOG_PATH
-
-echo "\n---- Log directory created sucessfully! ----"
-echo $LOG_PATH
-
+create_directory $LOG_PATH
 
 echo "\n---- Create Config directory ----"
 
 CONFIG_PATH="/$OE_HOME/config"
-sudo mkdir $CONFIG_PATH
+create_directory $CONFIG_PATH
 
-echo "\n---- Config directory created sucessfully! ----"
-echo $CONFIG_PATH
+sudo chown 101 $CONFIG_PATH
+
+sudo cp ./default_config.conf $CONFIG_PATH/odoo.conf
 
 echo "\n---- Create Addons directory ----"
 
 ADDONS_DIR="/$OE_HOME/extra-addons"
-sudo mkdir $ADDONS_DIR
-
-echo "\n---- Addons directory created sucessfully! ----"
-echo $ADDONS_DIR
-
-echo "\n---- Create Filestore directory ----"
-
-FILESTORE_DIR="/$OE_HOME/OdooData"
-sudo mkdir $FILESTORE_DIR
-
-sudo chown 101 $FILESTORE_DIR
-
-echo "\n---- Filestore directory created sucessfully! ----"
-echo $FILESTORE_DIR
+create_directory $ADDONS_DIR
 
 echo "\n---- Create Odoo container ----"
 
@@ -72,7 +68,15 @@ docker run -d \
     -v $CONFIG_PATH:/etc/odoo \
     -v $ADDONS_DIR:/mnt/extra-addons \
     -v $LOG_PATH:/var/log/odoo \
-    -v $FILESTORE_DIR:/var/lib/odoo \
+    -v odoo-web-data:/var/lib/odoo \
     --name $ODOO_CONTAINER_NAME \
     --link $POSTGRES_CONTAINER_NAME:$POSTGRES_CONTAINER_NAME \
     -t odoo:$ODOO_VERSION
+
+# Check the exit status of the last command (docker run)
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to run the Odoo container."
+    exit 1
+fi
+
+
